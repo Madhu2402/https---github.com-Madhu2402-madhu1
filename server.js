d62d6348598dfc867eb31bf91123bb1917e733e7
madhu <madhumithasaiboina@gmail.com>
2023-12-28 14:11:44 +0530

    changes

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.post('/', (req, res) => {
     res.sendFile(__dirname + '/index.html'); // Replace 'index.html' with your desired home page
 });
 
-app.post('/login', (req, res) => {
+app.post('https---github.com-Madhu2402-madhu1/login', (req, res) => {
     const enteredPassword = req.body.password;
 
     if (enteredPassword === correctPassword) {
